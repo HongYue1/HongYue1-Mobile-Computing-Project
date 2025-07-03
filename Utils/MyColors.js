c02fcd2a2f2d26fa625aebd3da5e725932e86447
Сказка <87040288+HongYue1@users.noreply.github.com>
2025-07-03 18:33:05 +0300

    Add files via upload

---
+++ Utils/MyColors.js
@@ -0,0 +1,21 @@
+export const MyColor = {
+  primary: "#03A791",
+  secondary: "#FFFFFF",
+  black: "#000000",
+  neutral: "gray",
+  neutral2: "#F2F3F2",
+  text: "#333333",
+  error: "#FF3B30",
+  success: "#4CD964",
+  warning: "#FF9500",
+  shadow: "rgba(0, 0, 0, 0.1)",
+  category: {
+    Fruits: "#FFB347", // orange-yellow
+    Vegetables: "#7ED957", // green
+    Dairy: "#F5F5DC", // light cream
+    Meat: "#FF6F61", // red
+    Seeds: "#BCAAA4", // brownish
+    AgriTech: "#4FC3F7", // blue
+    Default: "#03A791"
+  },
+};
